41edd0aa36469b621564260cf69d4b3287106825
bhavya khanna <bhavyakhanna88@gmail.com>
2021-03-09 18:15:50 +0530

    Factory created and seeded

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        //
+        "name" => $faker->word,
+        "price" => $faker->numberBetween(500, 10000),
+        "detail" => $faker->paragraph,
+        "stock" => $faker->randomDigit,
+        "discount" => $faker->numberBetween(5, 40)
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,15 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Product;
 
 $factory->define(App\Review::class, function (Faker $faker) {
     return [
-        //
+        'customer' => $faker->name,
+        'review' => $faker->paragraph(3, true),
+        'rating' => $faker->numberBetween(0, 5),
+        'product_id' => function () {
+            return Product::all()->random();
+        }
     ];
 });
